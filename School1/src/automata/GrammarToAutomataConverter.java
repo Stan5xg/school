bb99c16774b1e54883c00acfb5ba5a8a38850161
Stan5xg <sudostan@gmail.com>
2017-08-21 13:21:46 +0300

    work-in-progress

--- School1/src/automata/GrammarToAutomataConverter.java
+++ School1/src/automata/GrammarToAutomataConverter.java
@@ -2,20 +2,18 @@ package automata;
 
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 
 public class GrammarToAutomataConverter {
 
 
 	public static Automata convert(Grammar g) {
-		addNonterminal(g); //Шаг 1
 		
-		return null;
-	}
-
-	private static void addNonterminal(Grammar g) {
-		Set<Function> f = new HashSet<>();
-		Set<Character> z = new HashSet<>();
+		Set<Function> f = new HashSet<>(); //Множество правил
+		Set<Character> z = new HashSet<>(); //Множество заключительных состояний
+	
+		
 		for (Rule rule : g.p) {
 			if (rule.right.length == 2) {
 				f.add(new Function(rule.left,rule.right[0],rule.right[1]));
@@ -24,20 +22,48 @@ public class GrammarToAutomataConverter {
 			}
 			
 		}
+		
+		return null;
 	}
 
-	//Производит конструирование функции перехода для правил вида А->a (без нетерминала в правой части)   
-	private static void convertRuleWithTerminalOnly(Grammar g, Set<Character> z, Set<Character> f,  Rule rule) {
+	
+
+	//Производит конструирование функции перехода для правил вида А->a или S->ε	 
+	private static void convertRuleWithTerminalOnly(Grammar g, Set<Character> z, Set<Function> f,  Rule rule) {
+
 		if (isSToEmpty(g, rule)) {
 			z.add(rule.left);
 		} else {
-			for (Rule otherRule : g.p) {
-				if (sameExceptNonTerminal(rule, otherRule)) {
-					z.add(rule.right[1]);
-				}
+			convertRuleWithTerminalOnlyChecked(g, z, f, rule);
+		}
+	}
+
+	
+	//Производит конструирование функции перехода для правил вида А->a (без нетерминала в правой части)   
+	private static void convertRuleWithTerminalOnlyChecked(Grammar g, Set<Character> z, Set<Function> f, Rule rule) {
+		char resultState = getNewNonTerminal(g); //Новый символ 
+
+		for (Rule otherRule : g.p) {
+			if (sameExceptNonTerminal(rule, otherRule)) {
+				z.add(rule.right[1]);
+			} else {
+				f.add(new Function(rule.left,rule.right[0],resultState));
 			}
 		}
 	}
+
+	//Возвращает новый нетерминал, не содержавшийся в грамматике.  
+	private static char getNewNonTerminal(Grammar g) {
+		char result = 'N';
+		Random r = new Random();
+		while (g.vN.contains(result)) {
+			int charInt = 'A' + r.nextInt('Z' - 'A' + 1);
+			result = (char)charInt; 
+		}
+		return 0;
+	}
+
+
 
 	//Сравнивает два метода чтобы определить, является ли otherRule правилом вида A->aB если rule - правило вида A->a 
 	private static boolean sameExceptNonTerminal(Rule rule, Rule otherRule) {
@@ -47,7 +73,7 @@ public class GrammarToAutomataConverter {
 		return (otherRule.left == rule.left) && (otherRule.right[0] == rule.right[0]);
 	}
 
-	//Является ли правило rule правило вида S->ε
+	//Является ли правило rule правилом вида S->ε
 	private static boolean isSToEmpty(Grammar g, Rule rule) {
 		if (rule.right.length != 1) {
 			return false;
